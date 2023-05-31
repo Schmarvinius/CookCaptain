3c1fddbde48665b30606389b7807a37a3bd6dbd2
Til Weber <78653138+tilwbr@users.noreply.github.com>
2023-05-31 22:05:33 +0200

    design adjustements

--- clientside/src/LoginSignUp/Login.js
+++ clientside/src/LoginSignUp/Login.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
 
 function MyLogin() {
+
+    const [isSignUp, setIsSignUp] = useState(true);
+
+    function handleLogin(){
+        alert("login");
+    }
+    function handleSignUp(){
+        alert("signup");
+    }
+
     return (
         <div class="login-container">
-            <h2>Login</h2>
+            <h2>{isSignUp ? "Create new Account": "Login with username/email"}</h2>
             <form>
-                <input type="text" placeholder="username" required></input>
+                {isSignUp && <input type="text" placeholder="email"></input> }
+                
+                <input type="text" placeholder={isSignUp ? "username" : "username/email"} ></input>
                 <br/>
-                <input type="password" placeholder="password" required></input>
+                <input type="password" placeholder="password" ></input>
                 <br/>
-                <button type="submit">Login</button>
+                <button onClick={isSignUp? handleSignUp : handleLogin}type="submit">{isSignUp ? "Signup" : "Login"}</button>
+                <br/>
+                {/* <p  onClick={() => setIsSignUp(prevValue => !prevValue)}>{isSignUp ? "Already have account?": "Create Account"}</p> */}
+                <text>{isSignUp ? "Already have an account?" : "Don't have an account?"}</text>
+                <button type="button"  onClick={() => setIsSignUp(prevValue => !prevValue)}>{isSignUp ? "Log in": "Sign up"}</button>
             </form>
         </div>
     );
