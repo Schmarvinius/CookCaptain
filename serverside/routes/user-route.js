9d8f5002d38943ef254ff9d45648da68ad22ffbb
Marvin Lindner <mrvnlindner@gmail.com>
2023-06-01 12:40:10 +0200

    Added login for serverside

--- serverside/routes/user-route.js
+++ serverside/routes/user-route.js
@@ -2,14 +2,16 @@ const express = require('express');
 const { addUser} = require('../controllers/user/create-user-controller');
 const { getUserByID, getUserByEmail } = require('../controllers/user/get-user-controller');
 const { deleteUser } = require('../controllers/user/delete-user-controller');
-const { updateUser } = require('../controllers/user/update-user-controller')
+const { updateUser } = require('../controllers/user/update-user-controller');
+const { login } = require('../controllers/user/login-controller');
 
 const router = express.Router();
-router.post('/create', addUser);
+router.post('/', addUser);
 router.get('/Id',getUserByID);
 router.get('/Email',getUserByEmail);
 router.delete('/',deleteUser);
-router.post('/update',updateUser);
+router.patch('/',updateUser);
+router.post('/login', login);
 
 
 module.exports = router;
