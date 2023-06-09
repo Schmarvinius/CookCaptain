b3ac9331dee93f6f7e4bf04f4eb0c063d5bff00c
Marvin Lindner <mrvnlindner@gmail.com>
2023-06-09 10:32:06 +0200

    Created Recipe are now added to the user

--- serverside/controllers/recipes/create-recipe-controller.js
+++ serverside/controllers/recipes/create-recipe-controller.js
@@ -1,12 +1,25 @@
 const {sRecipe} = require('../../model/recipe-model.js');
+const { sUser } = require('../../model/user-model.js');
 const {errorHandler} = require('../errorHandler');
 
 const addRecipe = async (req,res) => {
     const newRecipe = new sRecipe(req.body);
     newRecipe.save()
-    .then((newRecipe) => {
-      console.log('Document saved successfully:', newRecipe);
-      return res.status(200).json(newRecipe);
+    .then(async (newRecipe) => {
+      await sUser.findOneAndUpdate({ email: newRecipe._doc.author },{ $addToSet: {createdRecipes: newRecipe.id}})
+                .then((updatedUser) => {
+                    if (updatedUser) {
+                      console.log('Document saved successfully:', newRecipe);
+                      return res.status(200).json(newRecipe);
+                    } else {
+                        console.log('User not found.');
+                        res.status(404).json({ error: 'User not found' });
+                    }
+                })
+                .catch((err) => {
+                    errorHandler(err,res);
+                })
+      
     })
     .catch((error)=>  {
         errorHandler(error, res); 
