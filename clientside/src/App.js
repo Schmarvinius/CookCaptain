896d2b800e7d5b3bf04ba896159ae0dd5c356ac1
“Kronprinz03” <87710293+Kronprinz03@users.noreply.github.com>
2023-06-12 11:31:40 +0200

    Team sagt meine UI war shit

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -1,9 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import MyLogin from './LoginSignUp/Login';
-import RecommandationPage from './MainViews/RecommendationPage.js';
-import SearchPage from './MainViews/SearchPage.js';
-import SafedRecipePage from './MainViews/SafedRecipePage.js';
+import HomePage from './MainViews/SafedRecipePage.js';
 import Lowbar from './Lowbar/Lowbar';
 import Imprint from './Imprint/Imprint';
 import { UserProvider } from './Context/UserContext';
@@ -18,9 +16,7 @@ function App() {
       <UserProvider>
         <Routes>
           <Route path="/" element={<MyLogin />} />
-          <Route path="/recommandation" element={<RecommandationPage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/safed" element={<SafedRecipePage />} />
+          <Route path="/home" element={<HomePage/>} />
           <Route path="/impressum" element={<Imprint />} /> 
         </Routes>
         <Lowbar/>
