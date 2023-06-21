617b983afbeee7e736410a8200974cd425a0fc99
Marvin Lindner <mrvnlindner@gmail.com>
2023-06-21 19:06:11 +0200

    added get request for likes and created recipes

---
+++ serverside/controllers/profile/sl-recipes-controller.js
@@ -0,0 +1,77 @@
+const { errorHandler, bodyerror } = require('../errorHandler.js');
+const mongoose = require('mongoose');
+
+const { sRecipe } = require('../../model/recipe-model.js');
+const { sUser } = require('../../model/user-model.js');
+
+const getLikedRecipe = async (req,res) => { 
+    if (!bodyerror(["email"],req.body)) {
+        return res.status(400).send(`Some required data wasn't passed`);
+    }
+
+    email = req.body.email;
+    sUser.find({email: email})
+    .then(async (fetchedUser) => {
+        if (fetchedUser) {
+            var recipes = new Array();
+            for (recipe of fetchedUser[0].likedRecipes) {
+                await sRecipe.find({_id: recipe})
+                .then((fetchedRecipe) => {
+                    if (fetchedRecipe && fetchedRecipe.length > 0) {
+                        recipes.push(fetchedRecipe[0]);
+                    }
+                })
+                .catch((err) => {
+                    errorHandler(err,res);
+                })
+            }
+            
+            return res.status(200).send(recipes);
+        } else {
+            return res.status(404).send(`User with ID: ${email} doesn't exist`);
+        }
+    })
+    .catch((err) => {
+        errorHandler(err,res);
+    })
+}
+
+const getCreatedRecipe = async (req,res) => {
+    if (!bodyerror(["email"],req.body)) {
+        return res.status(400).send(`Some required data wasn't passed`);
+    }
+
+    email = req.body.email;
+    sUser.find({email: email})
+    .then(async (fetchedUser) => {
+        if (fetchedUser) {
+            //check if isArray else crash
+            var recipes = new Array();
+            for (recipe of fetchedUser[0].createdRecipes) {
+                if (mongoose.Types.ObjectId.isValid(recipe)) {
+                    await sRecipe.find({_id: recipe})
+                    .then((fetchedRecipe) => {
+                        if (fetchedRecipe && fetchedRecipe.length > 0) {
+                            recipes.push(fetchedRecipe[0]);
+                        }
+                    })
+                    .catch((err) => {
+                        errorHandler(err,res);
+                    })
+                }
+                
+            }
+            return res.status(200).send(recipes);
+        } else {
+            return res.status(404).send(`User with ID: ${email} doesn't exist`);
+        }
+    })
+    .catch((err) => {
+        errorHandler(err,res);
+    })
+}
+
+module.exports = {
+    getCreatedRecipe,
+    getLikedRecipe
+}
