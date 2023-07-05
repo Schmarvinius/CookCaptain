27475137fc5f8dab39182d37369a4532fd736739
Til Weber <78653138+tilwbr@users.noreply.github.com>
2023-07-05 09:43:50 +0200

    updated imprint

--- clientside/src/Imprint/Imprint.js
+++ clientside/src/Imprint/Imprint.js
@@ -19,21 +19,56 @@ function Imprint() {
         <div className="background">
           <div className="headbar">
             <button className="textButton" onClick={handleBackClick}>
-              <div className="fancy">Cook Kaptain</div>
+              <div className="fancy">Cook Captain</div>
             </button>
           </div>
           <div className="imprintBody">
             <div className="impressum-content">
-              <h1>Impressum</h1>
-              <h2 className="impressum-heading">Contact Information</h2>
-              <p>Cook Captain</p>
-              <p>Address: Erzbergerstraße 12, Karlsruhe, Germany</p>
-              <p>Phone: +1 234 567 890</p>
-              <p>Email: CookCaptain@gmail.com</p>
-              <h2 className="impressum-heading">Legal Information</h2>
-              <p>Registration Number: 1234567</p>
-              <p>VAT Number: 1234567</p>
-              <p>Legal Disclaimer: Please dont sue us</p>
+              <p>
+                <b>Verantwortlich für den Inhalt dieser Website gemäß §5 TMG:</b>
+                <br/>Cook Captain GmbH
+                <br/>Nancystraße 24 
+                <br/>76187 Karlsruhe
+              </p>
+
+              <p>
+                <b>Vertreten durch:</b>
+                <br/> Marvin Lindner, Yannis Moser und Til Weber
+              </p>
+
+              <p>
+                <b>Kontakt:</b>
+                <br/>Telefon: +49 (0)187 45772
+                <br/>E-Mail: support@cookcaptain.de
+              </p>
+
+              <p>
+                <b>Handelsregister:</b>
+                <br/>Registergericht: Amtsgericht Karlsruhe
+                <br/>Registernummer: HRB 77815
+              </p>
+
+              <p>
+                <b>Umsatzsteuer-ID:</b>
+                <br/>Umsatzsteuer-Identifikationsnummer gemäß § 27a UStG: DE 987654321
+              </p>
+
+              <p>
+                <b>Verantwortlich für journalistisch-redaktionelle Inhalte gemäß § 55 RStV:</b>
+                <br/>Til Weber
+                <br/>Ringerottstraße 17
+                <br/>45772 Marl
+              </p>
+
+              <p>
+                <b>Haftungsausschluss:</b>
+                <br/>Die Betreiber der Website übernehmen keine Haftung für die Inhalte und der Korrektheit der bereitgestellten Rezepte. Für den Inhalt der Rezepte sind ausschließlich deren Ersteller verantwortlich.
+              </p>
+
+              <p>
+                <b>Urheberrecht:</b>
+                <br/>Die auf dieser Website (und von uns) verwendeten Texte, Bilder und Grafiken unterliegen dem deutschen Urheberrecht. Eine Vervielfältigung, Bearbeitung, Verbreitung oder jede Art der Verwertung außerhalb der Grenzen des Urheberrechts bedarf der schriftlichen Zustimmung der CookCaptain GmbH oder des jeweiligen Rechteinhabers.
+              </p>
             </div>
           </div>
         </div>
